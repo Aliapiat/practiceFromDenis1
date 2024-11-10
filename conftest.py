3f70a2ebfd1f0527de5d6adf7908bcebd5875631
Denis Tadzhibaev <incinerator88@mail.ru>
2024-11-10 22:47:53 +0300

    feat: lesson 10

--- conftest.py
+++ conftest.py
@@ -3,17 +3,25 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 
+from functions import get_root_path
+
+download_path = get_root_path("data\download")
 
 @pytest.fixture
 def driver():
     service = Service(executable_path=ChromeDriverManager().install())
     options = webdriver.ChromeOptions()
-    options.add_argument("--window-size=1900,1000")
+    prefs = {
+        "download.default_directory": download_path
+    }
+
+    options.add_argument("--window-size=1600,1000")
+    options.add_experimental_option("prefs", prefs)
     # options.add_argument("--headless")
-    options.add_argument("--incognito")
+    # options.add_argument("--incognito")
     # options.add_argument("--disable-cache")
     # options.add_argument("--start-maximized")
-    options.add_argument("--ignore-certificate-errors")
+    # options.add_argument("--ignore-certificate-errors")
     # options.page_load_strategy = "normal"
     # options.page_load_strategy = "eager"
     # options.page_load_strategy = "none"
