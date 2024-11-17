839cc668fd11ff07fa2a9e76efdeee50794fa23f
Denis Tadzhibaev <incinerator88@mail.ru>
2024-11-17 23:07:02 +0300

    feat: lesson 11

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,12 +1,12 @@
+from selenium.webdriver.ie.webdriver import WebDriver
 from selenium.webdriver.support.ui import WebDriverWait as wait
 from selenium.webdriver.support import expected_conditions as EC
-
-from conftest import driver
+from selenium.webdriver import ActionChains
 
 
 class BasePage:
 
-    def __init__(self, driver, url):
+    def __init__(self, driver: WebDriver, url: str):
         self.driver = driver
         self.url = url
 
@@ -24,3 +24,28 @@ class BasePage:
 
     def alert_is_visible(self, timeout=10):
         wait(self.driver, timeout=timeout).until(EC.alert_is_present())
+
+    def double_click(self, elem):
+        action = ActionChains(self.driver)
+        action.double_click(elem)
+        action.perform()
+
+    def right_click(self, elem):
+        action = ActionChains(self.driver)
+        action.context_click(elem)
+        action.perform()
+
+    def action_move_to_element(self, elem):
+        action = ActionChains(self.driver)
+        action.move_to_element(elem)
+        action.perform()
+
+    def action_drag_and_drop(self, source_elem, target_elem):
+        action = ActionChains(self.driver)
+        action.drag_and_drop(source_elem, target_elem)
+        action.perform()
+
+    def action_drag_and_drop_by_offset(self, elem, xoffset, yoffset):
+        action = ActionChains(self.driver)
+        action.drag_and_drop_by_offset(elem, xoffset, yoffset)
+        action.perform()
